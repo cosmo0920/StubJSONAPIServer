4029f40e656de723312a0434d8ecd73eafb647a6
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-06-12 23:51:42 +0900

    Use config value in test

--- src/test/scala/net/cosmo0920/app/StubJSONAPIServerServletSpec.scala
+++ src/test/scala/net/cosmo0920/app/StubJSONAPIServerServletSpec.scala
@@ -3,17 +3,22 @@ package net.cosmo0920.app
 import org.scalatra.test.specs2._
 import com.typesafe.config._
 
-// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class StubJSONAPIServerServletSpec extends ScalatraSpec { def is =
-  "GET / on StubJSONAPIServerServlet"                     ^
-    "should return status 200"                  ! root200^
-                                                end
-
-  addServlet(classOf[StubJSONAPIServerServlet], "/*")
+trait LoadConfig {
   val applicationConfig = ConfigFactory.load()
   val appURI = applicationConfig.getString("default.route")
+  val contents = applicationConfig.getString("default.response")
+}
 
+// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
+class StubJSONAPIServerServletSpec extends ScalatraSpec
+    with LoadConfig { def is = s2"""
+  GET $appURI on StubJSONAPIServerServlet
+    returns status 200           $root200
+  """
+
+  addServlet(classOf[StubJSONAPIServerServlet], "/*")
   def root200 = get(appURI) {
     status must_== 200
+    body must_== contents
   }
 }
